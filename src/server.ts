76d7528c56fb519289789aaf0718db2728144ff1
Johnny Wevany <johnny.wevany@moredev.tech>
2022-11-01 01:15:48 -0300

    starting the backend

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors';
+import Fastify from 'fastify';
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  })
+
+  await fastify.register(cors, {
+    origin: true,
+  })
+
+  fastify.get('/pools/count', async () => {
+    const pools = await prisma.pool.count()
+    return pools
+  })
+
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
+}
+
+bootstrap();
